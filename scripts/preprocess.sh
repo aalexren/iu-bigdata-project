cc8a5589c0d465e01f43ab335f27567d28a9825f
aalexren <chernicaar@gmail.com>
2023-04-11 23:16:58 +0300

    update: added preprocess.sh - downloading data files

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+parentdir=`pwd`
+
+printf "Downloading data files...\n\n\n"
+
+FILE="$parentdir"/data/application_data.csv
+if test -f "$FILE"; then
+    printf "$FILE exists.\n"
+    printf "Remove $FILE.\n"
+    rm $FILE
+    printf "Done.\n"
+fi
+printf "$FILE is downloading...\n"
+wget application_data.csv -P "$parentdir"/data/ https://media.githubusercontent.com/media/aalexren/iu-bigdata-project/master/data/application_data.csv
+printf "Done.\n\n"
+
+FILE="$parentdir"/data/previous_application.csv
+if test -f "$FILE"; then
+    printf "$FILE exists.\n"
+    printf "Remove $FILE.\n"
+    rm $FILE
+    printf "Done.\n"
+fi
+printf "$FILE is downloading...\n"
+wget previous_application.csv -P "$parentdir"/data/ https://media.githubusercontent.com/media/aalexren/iu-bigdata-project/master/data/previous_application.csv
+printf "Done.\n\n"
+
+printf "Downloading complete!\n\n\n"
