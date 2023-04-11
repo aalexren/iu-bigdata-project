9edd0c0b40b5915bceb4a117184067b4155c9337
aalexren <chernicaar@gmail.com>
2023-04-12 01:25:48 +0300

    update: add dump loading from csv to stage1.sh

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -3,16 +3,31 @@
 parentdir=`pwd`
 
 FILE="$parentdir"/sql/drop_database.sql
-printf "Drop database...\n\n\n"
+printf "Drop database...\n"
 PGPASSWORD=proven psql -U postgres < $FILE
 printf "Success!\n\n\n"
 
 FILE="$parentdir"/sql/create_database.sql
-printf "Create database...\n\n\n"
+printf "Create database...\n"
 PGPASSWORD=proven psql -U postgres < $FILE
 printf "Success!\n\n\n"
 
 FILE="$parentdir"/sql/create_tables.sql
-printf "Create tables...\n\n\n"
+printf "Create tables...\n"
 PGPASSWORD=proven psql -U postgres < $FILE
+printf "Success!\n\n\n"
+
+printf "Load data from csv files...\n"
+printf "Set datetime format...\n"
+PGPASSWORD=proven psql -U postgres \
+    -d project \
+    -c "SET datestyle TO iso, ymd;"
+printf "Load application_data.csv...\n"
+PGPASSWORD=proven psql -U postgres \
+    -d project \
+    -c "\COPY application_data FROM "$parentdir"/data/application_data.csv DELIMITER ',' CSV HEADER;"
+printf "Load previous_application.csv...\n"
+PGPASSWORD=proven psql -U postgres \
+    -d project \
+    -c "\COPY previous_application FROM "$parentdir"/data/previous_application.csv DELIMITER ',' CSV HEADER;"
 printf "Success!\n\n\n"
