aa090b95bf8dffd7f6b80ae0aaab9892a9f85a84
Extrabution <super.vanya-inchin-2002@ya.ru>
2023-04-16 02:17:00 +0300

    update: deleted duplicate of code (lines 12 to 21)

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -9,16 +9,6 @@ printf "INSTALL PYTHON REQUIREMENTS\n"
 pip install -r requirements.txt
 printf "SUCCESS!\n\n\n"
 
-DIR="$parent_dir/data/application_data"
-FILE="$DIR.csv"
-if test -f "$FILE"; then
-    printf "$FILE EXISTS!\n"
-    printf "REMOVE $FILE!\n"
-    rm -f $FILE
-    printf "SUCCESS!\n"
-fi
-python "$parent_dir/scripts/data/load.py" $data_dir $DIR
-printf "\n\n\n"
 
 DIR="$parent_dir/data/application_data"
 FILE="$DIR.csv"
