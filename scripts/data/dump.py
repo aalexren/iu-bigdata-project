e49d12eb350a4eef9be2fd5161545e8f90240a90
aalexren <chernicaar@gmail.com>
2023-05-07 21:17:29 +0300

    fixup: reformatted

--- scripts/data/dump.py
+++ scripts/data/dump.py
@@ -8,25 +8,27 @@ import os
 
 kilobytes = 1024
 megabytes = kilobytes * 1000
-chunksize = int(1.4 * megabytes)                   # default: roughly a floppy
+chunksize = int(1.4 * megabytes)  # default: roughly a floppy
 
-def split(fromfile, todir, chunksize=chunksize): 
-    if not os.path.exists(todir):                  # caller handles errors
-        os.mkdir(todir)                            # make dir, read/write parts
+
+def split(fromfile, todir, chunksize=chunksize):
+    if not os.path.exists(todir):  # caller handles errors
+        os.mkdir(todir)  # make dir, read/write parts
     else:
-        for fname in os.listdir(todir):            # delete any existing files
-            os.remove(os.path.join(todir, fname)) 
+        for fname in os.listdir(todir):  # delete any existing files
+            os.remove(os.path.join(todir, fname))
     partnum = 0
-    
-    input = open(fromfile, 'rb')                   # use binary mode on Windows
-    while 1:                                       # eof=empty string from read
-        chunk = input.read(chunksize)              # get next part <= chunksize
-        if not chunk: break
-        partnum  = partnum+1
-        filename = os.path.join(todir, ('part%03d' % partnum))
-        fileobj  = open(filename, 'wb')
+
+    input = open(fromfile, "rb")  # use binary mode on Windows
+    while 1:  # eof=empty string from read
+        chunk = input.read(chunksize)  # get next part <= chunksize
+        if not chunk:
+            break
+        partnum = partnum + 1
+        filename = os.path.join(todir, ("part%03d" % partnum))
+        fileobj = open(filename, "wb")
         fileobj.write(chunk)
-        fileobj.close()                            # or simply open(  ).write(  )
+        fileobj.close()  # or simply open(  ).write(  )
     input.close()
-    
+
     return partnum
--- scripts/data/load.py
+++ scripts/data/load.py
@@ -9,16 +9,18 @@ import sys
 
 readsize = 1024
 
+
 def join(fromdir, tofile):
-    output = open(tofile, 'wb')
-    parts  = os.listdir(fromdir)
+    output = open(tofile, "wb")
+    parts = os.listdir(fromdir)
     parts.sort()
     for filename in parts:
         filepath = os.path.join(fromdir, filename)
-        fileobj  = open(filepath, 'rb')
+        fileobj = open(filepath, "rb")
         while 1:
             filebytes = fileobj.read(readsize)
-            if not filebytes: break
+            if not filebytes:
+                break
             output.write(filebytes)
         fileobj.close()
     output.close()
