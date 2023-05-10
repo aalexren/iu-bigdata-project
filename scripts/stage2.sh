6aa354e43ca19f636acd6c738696d4e3df651641
aalexren <chernicaar@gmail.com>
2023-05-10 23:25:54 +0300

    fixup: rm projectdb folder before stage2 is executed

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -2,6 +2,9 @@
 
 parentdir=`pwd`
 
+hdfs dfs -rm -r /project/projectdb
+hdfs dfs -mkdir /project/projectdb
+
 printf "LOAD DATA TO HIVE\n"
 hive -f "$parentdir"/sql/projectdb.hql
 printf "SUCCESS!\n\n\n"
