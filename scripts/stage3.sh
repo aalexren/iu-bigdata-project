c8efa323cc2e43bc5149aaad50be637b04e272ba
aalexren <chernicaar@gmail.com>
2023-05-11 09:49:31 +0300

    fixup: python encoding

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PYTHONIOENCODING=utf8;
+
 spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/model.py
 
 cat output/lr_scores/* >> output/lr_scores.csv
