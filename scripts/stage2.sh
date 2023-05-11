a7c4b5d5703a8e593a871c50ee16e82dd46c1143
aalexren <chernicaar@gmail.com>
2023-05-11 15:07:48 +0300

    fixup: insights stage2

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -9,3 +9,18 @@ printf "LOAD DATA TO HIVE\n"
 hive -f "$parentdir"/sql/create_database.hql
 hive -f "$parentdir"/sql/partition_tables.hql
 printf "SUCCESS!\n\n\n"
+
+
+printf "INSIGHTS\n"
+rm -rf "$parentdir"/output/q1
+rm -rf "$parentdir"/output/q1.csv
+rm -rf "$parentdir"/output/q2
+rm -rf "$parentdir"/output/q2.csv
+rm -rf "$parentdir"/output/q3
+rm -rf "$parentdir"/output/q3.csv
+rm -rf "$parentdir"/output/q4
+rm -rf "$parentdir"/output/q4.csv
+rm -rf "$parentdir"/output/q5
+rm -rf "$parentdir"/output/q5.csv
+hive -f "$parentdir"/sql/insights.hql
+printf "SUCCESS!\n\n\n"
